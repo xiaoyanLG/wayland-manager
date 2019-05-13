41a0f1490621b4340b56103eb35d15f6cc0736b7
luogang <luogang@acoinfo.com>
2019-05-13 16:48:35 +0800

    增加对事件队列用法的说明！

--- examples/Qt/wayland-client/main.c
+++ examples/Qt/wayland-client/main.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
-#include "wayland-client.h"
+#include <wayland-client.h>
 
 static void
 registry_handle_global(void *data, struct wl_registry *registry,
@@ -21,7 +21,7 @@ registry_handle_global(void *data, struct wl_registry *registry,
                          id, &wl_shm_interface, 1);
 
 
-            int fd = open("/home/rootqt/QtProjects/build-wayland-client-Desktop_Qt_5_9_5_GCC_64bit-Release/test.txt", O_RDWR);
+            int fd = open("test.txt", O_RDWR | O_APPEND);
             struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, 50000);
             wl_shm_pool_create_buffer(pool, 0, 5, 5, 500, 0);
             wl_display_roundtrip(display);
@@ -32,6 +32,7 @@ static void
 registry_handle_global_remove(void *data, struct wl_registry *registry,
                   uint32_t name)
 {
+    printf("func: %s, line: %d\n", __FUNCTION__, __LINE__);
 }
 
 static const struct wl_registry_listener registry_listener = {
@@ -39,22 +40,57 @@ static const struct wl_registry_listener registry_listener = {
     registry_handle_global_remove
 };
 
+static void
+call_back_handle_done(void *data,
+                      struct wl_callback *wl_callback,
+                      uint32_t callback_data)
+{
+    struct wl_event_queue {
+        struct wl_list event_list;
+        struct wl_display *display;
+    };
+
+    struct wl_event_queue *newq = data;
+
+    wl_display_roundtrip_queue(newq->display, newq);
+    printf("func: %s, line: %d\n", __FUNCTION__, __LINE__);
+}
+
+static const struct wl_callback_listener call_back_listener = {
+    call_back_handle_done
+};
+
 int main()
 {
-    printf("Wayland Client! 1111\n");
+    printf("build time: %s-%s, func: %s, line: %d\n", __DATE__, __TIME__, __FUNCTION__, __LINE__);
+
     struct wl_display *display = wl_display_connect(NULL);
-	
-printf("Wayland Client! line: %d\n", __LINE__);
+
     struct wl_registry *registry = wl_display_get_registry(display);
-printf("Wayland Client! line: %d\n", __LINE__);
+
+    // 如果指定了自己的事件队列，那么wl_display_dispatch派发的时候就不会派发该队列的事件，
+    // 需要自己调用wl_display_dispatch_queue明确指定需要派发的事件队列，所有的代理对象（wl_proxy）
+    // 都有一个事件队列，如果没有明确指定都是从创建该代理对象的父对象继承过来的，最原始的是wl_display
+    // 对象的事件队列，也就是wl_display->default_queue队列
+    struct wl_event_queue *newq = wl_display_create_queue(display);
+    wl_proxy_set_queue((struct wl_proxy *)registry, newq);
+
     wl_registry_add_listener(registry,
                      &registry_listener, display);
-printf("Wayland Client! line: %d\n", __LINE__);
+
+    struct wl_callback *cl = wl_display_sync(display);
+    wl_callback_add_listener(cl, &call_back_listener, newq);
+
+    // 如果使用下面的代码，那么就不会触发任何事件的派发了，就因为指定了另外的事件队列
+//    newq = wl_display_create_queue(display);
+//    wl_proxy_set_queue((struct wl_proxy *)cl, newq);
+
     wl_display_roundtrip(display);
-printf("Wayland Client! line: %d\n", __LINE__);
+
     int ret = 1;
-    while (ret) {
+    do {
         ret = wl_display_dispatch(display);
-    }
+    }while (ret);
+
     return 0;
 }
